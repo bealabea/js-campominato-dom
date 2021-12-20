3c97993940de9950de6d1bd12968c6bf322e23db
bealabea <beatricemarin89@yahoo.it>
2021-12-20 22:41:39 +0100

    generates the message HAI VINTO and the button to reload the page

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ function buildGrid(output, maxNum, width){
     output.className = width;
     numbers = [];
     for (let i = 1; i <= maxNum; i++) {
-        createNewBox(containerHtml, i);
+        createNewBox(containerHtml, maxNum, i);
         while (numbers.length < 16) {
             let randomNumber = getRandom(1, maxNum);
             if (!numbers.includes(randomNumber)){
@@ -26,34 +26,41 @@ function buildGrid(output, maxNum, width){
     }
 }
 
+
+
 // Creo una funzione per creare un box e la colorazione del box al click (con la funzione colorClick)
-function createNewBox (output, num) {
+function createNewBox (output, maxNum, num) {
     let newSquare = document.createElement('div');
     newSquare.className ='box';
     newSquare.innerHTML = num;
     output.append(newSquare);
-    colorClick(newSquare, num);
+    colorClick(newSquare, maxNum, num);
 }
 
-
+let blueBox = 0;
 // Creo una nuova funzione per la colorazione dei box
-function colorClick(box, i) {
+function colorClick(box, maxNum, i) {
     box.addEventListener('click', function() {
-        this.classList.add('blue');
-        // se l'array numbers (in cui ho generato 16 numeri random) include il numero corrispondente al box cliccato, questo diventa rosso
-        if(numbers.includes(i)){
-           this.classList.add('red');
-           this.classList.remove('blue');
-        // aggiungo la funzione per rimuovere la classe hide e quindi mostrare GAME OVER e il button per ricaricare la pagina
-           classShow(gameOverHtml, playAgainHtml);
+        
+        // se il box cliccato non contiene un numero corrispondente all'array numbers (in cui ho generato 16 numeri random) questo diventerà blu,
+        // e se il numero dei box blu raggiunge il numero massimo esclusi i numeri dell'array,
+        // allora uscirà il messaggio HAI VINTO e il button per ricaricare la pagina
+        if(!numbers.includes(i)){
+            this.classList.add('blue');
+            blueBox++
+            if (blueBox === (maxNum - 16)) {
+                winHtml.classList.remove('hide');
+                playAgainHtml.classList.remove('hide');
+            }
+        // ALTRIMENTI il numero del box cliccato corrisponde al numero contenuto nell'array quindi
+        // il box diventerà rosso e uscirà il messaggio GAME OVER e il button per ricaricare la pagina   
+        } else {
+            this.classList.add('red');
+            this.classList.remove('blue');
+            gameOverHtml.classList.remove('hide');
+            playAgainHtml.classList.remove('hide');
         }
     })
-}
-
-// funzione per rimuovere la classe hide
-function classShow(gameOver, playAgain){
-    gameOver.classList.remove('hide');
-    playAgain.classList.remove('hide');
 }
 
 // Assegno le variabili per i 3 button
